eef5a256e0a68ed69a5cc72344410c52173eb1aa
jianrong7 <ipwnjianrong@gmail.com>
2021-03-22 15:37:54 +0800

    Main functionality of game is completed

--- newScript.js
+++ newScript.js
@@ -4,15 +4,16 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 function computerPlay() {
+    let image = document.querySelector(".computerChoiceImage")
     let number = getRandomInt(0, 2)
     if (number == 0) {
-        console.log("rock")
+        image.innerHTML = '<img src="assets/rock.jpg" alt="rock" id="rock"></img>'
         return "rock"
     } else if (number == 1) {
-        console.log("paper")
+        image.innerHTML = '<img src="assets/paper.jpg" alt="paper" id="paper"></img>'
         return "paper"
     } else {
-        console.log("scissors")
+        image.innerHTML = '<img src="assets/scissors.jpg" alt="scissors" id="scissors"></img>'
         return "scissors"
     }
 };
@@ -44,37 +45,70 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 function determineWinner(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        return "you win"
-    } else if (computerScore > playerScore) {
-        return "you lose"
-    } else {
-        return "tie"
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            return "W"
+        } else {
+            return "L"
+        }
     }
 }
-function getPlayerSelection() {
-    playerSelection = prompt("What's you choice?", "rock")
-    playerSelection = playerSelection.toLowerCase()
-    return playerSelection
+function endGame(finalResult) {
+    let resultText = document.querySelector(".gameResult")
+    let restartButton = document.querySelector(".button")
+    if (finalResult == "W") {
+        resultText.innerHTML = "You won"
+        restartButton.style.visibility = "visible"
+    } else {
+        resultText.innerHTML = "You lost"
+        restartButton.style.visibility = "visible"
+    }
+}
+function resetGame() {
+    let restartButton = document.querySelector(".button")
+    restartButton.addEventListener('click', () => {
+        window.location.reload();
+    })
 }
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let rounds = 0
-    while (rounds < 5) {
-        let result = playRound(getPlayerSelection(), computerPlay())
-        
-        if (result == "player wins") {
-            playerScore++
-            rounds++
-        } else if (result == "computer wins") {
-            computerScore++
-            rounds++
-        } else {
-            rounds++
-        }
-        console.log(playerScore, computerScore)
-    }
+    let choices = document.querySelectorAll('.choice')
+    let playerSelection
+    let playerScore = 0
+    let computerScore = 0
+    let playerScoreHTML = document.querySelector('.pScore')
+    let computerScoreHTML = document.querySelector('.cSore')
+    choices.forEach((choice) => {
+        choice.addEventListener('click', () => {
+            if (choice.classList.contains('rock')) {
+                playerSelection = "rock"
+            } else if (choice.classList.contains('paper')) {
+                playerSelection = "paper"
+            } else {
+                playerSelection = "scissors"
+            }
+            
+            let result = playRound(playerSelection, computerPlay())
     
-    return determineWinner(playerScore, computerScore)
+            if (result == "player wins") {
+                playerScore++
+                playerScoreHTML.innerHTML = playerScore
+                console.log(playerScore, computerScore)
+            } else if (result == "computer wins") {
+                computerScore++
+                computerScoreHTML.innerHTML = computerScore
+                console.log(playerScore, computerScore)
+            } else {
+                console.log(playerScore, computerScore)
+            }
+            
+            let finalResult = determineWinner(playerScore, computerScore)
+            if (finalResult == "W") {
+                endGame(finalResult)
+            } else if (finalResult == "L") {
+                endGame(finalResult)
+            }
+            resetGame()
+        })
+    })
 }
+game()
