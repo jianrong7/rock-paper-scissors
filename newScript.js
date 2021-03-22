ac0070dda74a9a613a44da6effecd9607aa771b1
jianrong7 <ipwnjianrong@gmail.com>
2021-03-22 11:18:44 +0800

    Added playRound function

--- newScript.js
+++ newScript.js
@@ -7,15 +7,43 @@ function computerPlay() {
     let number = getRandomInt(0, 2)
     if (number == 0) {
         console.log("rock")
-        return
+        return "rock"
     } else if (number == 1) {
         console.log("paper")
-        return
+        return "paper"
     } else {
         console.log("scissors")
-        return
+        return "scissors"
     }
 };
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            console.log("tie")
+        } else if (computerSelection == "paper") {
+            console.log("computer wins")
+        } else {
+            console.log("player wins")
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            console.log("player wins")
+        } else if (computerSelection == "paper") {
+            console.log("tie")
+        } else {
+            console.log("computer wins")
+        }
+    } else { // player selects scissors
+        if (computerSelection == "rock") {
+            console.log("computer wins")
+        } else if (computerSelection == "paper") {
+            console.log("player wins")
+        } else {
+            console.log("tie")
+        }
+    }
+    return
+}
+function game() {
     
 }
