7c7e217bd37d4cebea0a476b20730730783e876c
jianrong7 <ipwnjianrong@gmail.com>
2021-03-22 11:26:45 +0800

    added game function

--- newScript.js
+++ newScript.js
@@ -6,44 +6,63 @@ function getRandomInt(min, max) {
 function computerPlay() {
     let number = getRandomInt(0, 2)
     if (number == 0) {
-        console.log("rock")
         return "rock"
     } else if (number == 1) {
-        console.log("paper")
         return "paper"
     } else {
-        console.log("scissors")
         return "scissors"
     }
 };
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            console.log("tie")
+            return "tie"
         } else if (computerSelection == "paper") {
-            console.log("computer wins")
+            return "computer wins"
         } else {
-            console.log("player wins")
+            return "player wins"
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            console.log("player wins")
+            return "player wins"
         } else if (computerSelection == "paper") {
-            console.log("tie")
+            return "tie"
         } else {
-            console.log("computer wins")
+            return "computer wins"
         }
     } else { // player selects scissors
         if (computerSelection == "rock") {
-            console.log("computer wins")
+            return "computer wins"
         } else if (computerSelection == "paper") {
-            console.log("player wins")
+            return "player wins"
         } else {
-            console.log("tie")
+            return "tie"
         }
     }
-    return
 }
-function game() {
-    
+function game(playerSelection) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let rounds = 0
+    while (rounds < 5) {
+        let result = playRound(playerSelection, computerPlay())
+        
+        if (result == "player wins") {
+            playerScore++
+            rounds++
+        } else if (result == "computer wins") {
+            computerScore++
+            rounds++
+        } else {
+            rounds++
+        }
+        console.log(playerScore, computerScore)
+    }
+    if (playerScore > computerScore) {
+        return "you win"
+    } else if (computerScore > playerScore) {
+        return "you lose"
+    } else {
+        return "tie"
+    }
 }
